cd91e8bf9b3195965191b45f7fa49fe63bda7b30
Sean Birmingham <seannbirmingham98@gmail.com>
2019-12-12 00:03:24 -0500

    created formik form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>User-Onboarding</h1>
       <Form />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,34 @@
-import React from "react";
-import { withFormik, Form, Field } from "formik";
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
 
 const SignUpForm = () => {
-    return (
-        <Form>
-        </Form>
-    )
-}
+  return (
+    <Form>
+      <label>
+        Name
+        <Field type='text' name='name' placeholder='Enter your name' />
+      </label>
+      <label>
+        Email
+        <Field type='text' name='email' placeholder='Enter your email' />
+      </label>
+      <label>
+        Password
+        <Field type='text' name='email' placeholder='Enter your password' />
+      </label>
+      <label>
+        Terms of Service
+        <Field type='checkbox' name='terms of service' />
+      </label>
+      <button type='submit'>Submit</button>
+    </Form>
+  );
+};
 
 const FormikForm = withFormik({
-    mapPropsToValues(props) {
-        return {
-
-        }
-    }
+  mapPropsToValues(props) {
+    return {};
+  }
 })(SignUpForm);
 
 export default FormikForm;
