dddad2399b70f794f89edd2afbaf32db83fa7b21
Sean Birmingham <seannbirmingham98@gmail.com>
2019-12-12 00:34:40 -0500

    added errors for name, email, and password

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,24 +2,29 @@ import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-const SignUpForm = () => {
+const SignUpForm = ({ values, errors, touched }) => {
+  //   console.log('errors', errors);
+  //   console.log('touched', touched);
   return (
     <Form>
       <label>
         Name
         <Field type='text' name='name' placeholder='Enter your name' />
+        {touched.name && errors && <p>{errors.name}</p>}
       </label>
       <label>
         Email
         <Field type='text' name='email' placeholder='Enter your email' />
+        {touched.email && errors && <p>{errors.email}</p>}
       </label>
       <label>
         Password
         <Field type='text' name='email' placeholder='Enter your password' />
+        {touched.email && errors && <p>{errors.password}</p>}
       </label>
       <label>
         Terms of Service
-        <Field type='checkbox' name='terms of service' />
+        <Field type='checkbox' name='terms' />
       </label>
       <button type='submit'>Submit</button>
     </Form>
@@ -36,9 +41,10 @@ const FormikForm = withFormik({
     };
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string().required(),
-    email: Yup.string().required(),
-    password: Yup.string().required()
+    name: Yup.string().required('Please enter your name'),
+    email: Yup.string().required('Please enter your email'),
+    password: Yup.string().required('Please enter a password'),
+    terms: Yup.boolean()
   })
 })(SignUpForm);
 
