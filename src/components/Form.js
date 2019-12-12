cddd8a93f49423a9ce28d11d629d66b5b98e3d89
Sean Birmingham <seannbirmingham98@gmail.com>
2019-12-12 00:19:51 -0500

    added validations for name, email, and password

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 const SignUpForm = () => {
   return (
@@ -27,8 +28,18 @@ const SignUpForm = () => {
 
 const FormikForm = withFormik({
   mapPropsToValues(props) {
-    return {};
-  }
+    return {
+      name: props.name || '',
+      email: props.email || '',
+      password: props.password || '',
+      terms: props.terms || false
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().required(),
+    password: Yup.string().required()
+  })
 })(SignUpForm);
 
 export default FormikForm;
